e3147870ca9c436ddad7d4e3eec880408262c8ae
Hari Prasad Dandu <hariprasad.dandu@smartron.com>
2018-05-14 12:51:09 +0530

    basic app with kotlin

--- app/src/main/java/com/hari/kotlinfirststep/MainActivity.kt
+++ app/src/main/java/com/hari/kotlinfirststep/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.hari.kotlinfirststep
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        submit_btn.setOnClickListener {
+            Toast.makeText(this, title_et.text.toString() + " " + body_et.text.toString(), Toast.LENGTH_LONG).show()
+        }
     }
 }
